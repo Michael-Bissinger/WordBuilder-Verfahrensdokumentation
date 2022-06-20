343c2ae0d95ff3f142f89b976d5665b483fb6a6a
Michael.Bissinger <Michael.Bissinger@inb.de>
2022-06-20 15:57:10 +0200

    First process

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args){
+        System.out.println("Ausgabe aus der main()-Methode");
+
+        // Wo liegen die benötigten Dokumente?
+
+        // Auslesen Datei XYZ
+
+
+    }
+}
