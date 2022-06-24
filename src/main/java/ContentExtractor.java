812e840c89b095c10262f53e2906953588b1601d
Michael.Bissinger <Michael.Bissinger@inb.de>
2022-06-24 20:06:13 +0200

    add lists for parts

---
+++ src/main/java/ContentExtractor.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class ContentExtractor {
+
+    public static List<String> getContent (List<String> list, String abc) {
+
+        // Datei raussuchen
+
+        // Auslesen und in Liste speichern (Zeile für Zeile)
+
+        return list;
+
+
+    }
+
+}
+
+
+
+
+
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -10,7 +10,14 @@ public class main {
 
         // Hier werden Zeilen für finalen Output gesammelt
         List<String> latex_dependencies = new ArrayList<>();
-        List<String> lines = new ArrayList<>();
+        List<String> titelblatt = new ArrayList<>();
+        List<String> vorbemerkungen = new ArrayList<>();
+        List<String> zielsetzung_ueberblick = new ArrayList<>();
+        List<String> organisation_sicherheit = new ArrayList<>();
+        List<String> verfahren_massnahmen = new ArrayList<>();
+        List<String> mitgeltende_unterlagen = new ArrayList<>();
+        List<String> aenderungshistorie = new ArrayList<>();
+        //List<String> lines = new ArrayList<>();
 
         System.out.println("File-Location ist: " + location);
 
@@ -23,8 +30,12 @@ public class main {
 
         DocumentFinalizer.createOutputDoc();
 
+        latex_dependencies = ContentExtractor.getContent(latex_dependencies,
+                "\"Ressourcen\"Style\"latex_dependencies\"");
+
+
         DocumentFinalizer.writeContent(latex_dependencies);
-        DocumentFinalizer.writeContent(lines);
+        DocumentFinalizer.writeContent(titelblatt);
 
 
 
